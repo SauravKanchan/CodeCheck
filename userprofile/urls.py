e914914eddd4a9ef3e6bf0ec158fe10ace15ead0
Shubham Nishad <shubhamnishad25@gmail.com>
2017-09-04 20:10:19 +0530

    made logout view

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -11,7 +11,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url('accounts/profile/',profile,name='profile'),
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', logout_view,name='logout'),
     url(r'^signup/$', signup, name='signup'),
 
 ]
