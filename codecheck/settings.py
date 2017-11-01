8012fa41141449bde50fc42ca75da78d9ef5fbd4
Owais <owaiswizard@gmail.com>
2017-11-01 13:59:41 +0530

    Revert settings.py to upstream's default

--- codecheck/settings.py
+++ codecheck/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ALLOWED_HOSTS = ['*']
